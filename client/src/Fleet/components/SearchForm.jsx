25e860ccc87338ba7ad6e97672f7406b4ac282d1
dhruvi0102 <dhruvibavaria2@gmail.com>
2023-12-06 10:13:50 -0500

    Order and Fleet

---
+++ client/src/Fleet/components/SearchForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+
+const SearchForm = (props) => {
+
+    const [ name, setName ] = useState('');
+
+    const onSubmitNameHandler = (e) => {
+        const convName = name.toLocaleLowerCase();
+        const arr = convName.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+        }
+        e.preventDefault();
+        const convName2 = arr.join(" ");
+        props.onSubmitNameHandler(convName2);
+        console.log(name);
+        setName('');
+        
+    };
+
+    return(
+        <div className='search-form'>
+            <form onSubmit={onSubmitNameHandler}>
+                    <input type='text' name='name' value={name} placeholder='Enter type or name of rental you are looking for'
+                    required onChange={(e) => setName(e.target.value)} />
+                    {
+                        props.onSearch 
+                        ? <button className='clear-search' onClick={props.onSearchClear} style = {{backgroundColor: 'white'}}>clear</button>
+                        : <input type='submit' value='search' style = {{backgroundColor: 'white'}} />
+                    }                   
+            </form>
+        </div>
+    )
+}
+
+export default SearchForm;
---
+++ client/src/shared/contexts/ThemeContext.jsx
@@ -0,0 +1,51 @@
+import React, { createContext, useState } from 'react';
+import img1 from '../../assets/randiimg.webp'
+import img2 from '../../assets/randiimg.jpeg'
+const color = '#1B263B';
+
+export const themes = { 
+    
+    light: {
+        backgroundColor: '#FFFFFF',
+        color: '#2F3C7E',
+       imagePeecheki: img2,
+       cardColor:  "rgba(215, 215, 215, 0.318)",
+       cardFontColor: '#2F3C7E',
+       cardBorderBottomColor:'black',
+       formColor : "white",
+       formContentColor: "#2F3C7E"
+    },
+    dark: {
+        backgroundColor: 'black',
+        color: '#FBEAEB',
+        imagePeecheki: img1,
+        cardColor: "white",
+        cardFontColor: '#2F3C7E',
+        cardBorderBottomColor:'red',
+        formColor : "red",
+        formContentColor: "white"
+
+    }
+    
+}
+
+export const ThemeContext = createContext(themes.light);
+
+const ThemeContextProvider = (props) => {
+
+    const [ currentTheme, setCurrentTheme ] = useState(themes.light);
+
+    const toggleTheme = () => {
+        currentTheme === themes.light ? 
+        setCurrentTheme(themes.dark):
+        setCurrentTheme(themes.light)
+    }
+
+    return (
+        <ThemeContext.Provider value={{currentTheme, toggleTheme}}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeContextProvider

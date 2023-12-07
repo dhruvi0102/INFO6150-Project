b64bb11b14d92bd0ae4e2d3efd95530b5486a682
heenas221000 <114357648+heenas221000@users.noreply.github.com>
2023-12-07 18:56:05 -0500

    Data

---
+++ backend/filldb/data.js
@@ -0,0 +1,174 @@
+const carData = [
+    {
+      name: "BMW M4",
+      rentalmodel: "book@bmw.com",
+      rentalType: "coupe",
+      rooms: "4",
+      bathrooms: "Automatic",
+      price: "999.99",
+      qt: "5",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/BMW_M4_%28G82%29_IMG_4183.jpg/1200px-BMW_M4_%28G82%29_IMG_4183.jpg",
+    },
+    {
+      name: "BMW M2",
+      rentalmodel: "manager@bmw.com",
+      rentalType: "coupe",
+      rooms: "4",
+      bathrooms: "Automatic",
+      price: "699.99",
+      qt: "7",
+      image:
+        "https://hips.hearstapps.com/hmg-prod/images/2023-bmw-m2-14-1665430075.jpg?crop=0.647xw:0.729xh;0.0896xw,0.169xh&resize=640:*",
+    },
+    {
+      name: "Mercedes E250",
+      rentalmodel: "manager@mercedes.com",
+      rentalType: "coupe",
+      rooms: "4",
+      bathrooms: "Automatic",
+      price: "699.99",
+      qt: "7",
+      image:
+        "https://www.autoguide.com/blog/wp-content/uploads/2015/02/2014-Mercedes-E250-bluetec-main.jpg",
+    },
+    {
+      name: "Audi A6",
+      rentalmodel: "manager@audi.com",
+      rentalType: "coupe",
+      rooms: "4",
+      bathrooms: "Automatic",
+      price: "899.99",
+      qt: "7",
+      image:
+        "https://hips.hearstapps.com/hmg-prod/images/small-5051-2019audia6-1651169671.jpg?crop=0.891xw:1.00xh;0.0603xw,0&resize=640:*",
+    },
+    {
+      name: "Aston Martin",
+      rentalmodel: "manage@astonmartin.com",
+      rentalType: "coupe",
+      rooms: "4",
+      bathrooms: "Automatic",
+      price: "1399.99",
+      qt: "10",
+      image:
+        "https://media.ed.edmunds-media.com/aston-martin/vantage/2021/oem/2021_aston-martin_vantage_convertible_roadster_fq_oem_1_815.jpg",
+    },
+    {
+      name: "Audi Q8",
+      rentalmodel: "manage@audi.com",
+      rentalType: "suv",
+      rooms: "7",
+      bathrooms: "Automatic",
+      price: "899.99",
+      qt: "10",
+      image:
+        "https://cdn.motor1.com/images/mgl/mrY7q/s3/audi-q8-tfsi-e-quattro-2020.jpg",
+    },
+    {
+      name: "BMW X6",
+      rentalmodel: "manage@bmw.com",
+      rentalType: "suv",
+      rooms: "7",
+      bathrooms: "Automatic",
+      price: "1999.99",
+      qt: "10",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/2/2a/2018_BMW_X6_xDrive30d_M_Sport_Automatic_3.0_Front.jpg",
+    },
+    {
+      name: "Pajero",
+      rentalmodel: "manage@mitshubishi.com",
+      rentalType: "suv",
+      rooms: "7",
+      bathrooms: "Automatic",
+      price: "299.99",
+      qt: "10",
+      image:
+        "https://www.motortrend.com/uploads/sites/11/2020/04/Mitsubishi-Montero-2.jpg?fit=around%7C875:492",
+    },
+    {
+      name: "MG Hector",
+      model: "manage@hector.com",
+      carType: "suv",
+      seats: "7",
+      gears: "Automatic",
+      price: "199.99",
+      qt: "7",
+      image:
+        "https://www.ccarprice.com/products/MG-Hector-Smart-Diesel.jpg",
+    },
+    {
+      name: "BMW 540i",
+      model: "book@bmw.com",
+      carType: "sedan",
+      seats: "4",
+      gears: "Automatic",
+      price: "999.99",
+      qt: "5",
+      image:
+        "https://i.ytimg.com/vi/peqAaevHbos/maxresdefault.jpg",
+    },
+    {
+      name: "BMW 320d",
+      model: "manager@bmw.com",
+      carType: "sedan",
+      seats: "4",
+      gears: "Automatic",
+      price: "699.99",
+      qt: "7",
+      image:
+        "https://s1.cdn.autoevolution.com/images/news/2019-bmw-320d-and-330i-take-0-100-km-h-acceleration-test-134737_1.jpg",
+    },
+    {
+      name: "Mercedes S500",
+      model: "manager@mercedes.com",
+      carType: "sedan",
+      seats: "4",
+      gears: "Automatic",
+      price: "699.99",
+      qt: "7",
+      image:
+        "https://s3.us-east-2.amazonaws.com/dealer-inspire-vps-vehicle-images/11000427/W1K6G6DB1PA180670/dcae2efdec7bdca0169cfc94e921318a.jpg",
+    },
+    {
+      name: "Audi A7L",
+      model: "manager@audi.com",
+      carType: "sedan",
+      seats: "4",
+      gears: "Automatic",
+      price: "899.99",
+      qt: "7",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/9/92/Audi_A7L_004.jpg",
+    },
+  ];
+  
+  const userData = [
+    {
+      name: "Jainam Mehta",
+      email: "mehta.jain@northeastern.edu",
+      password: "Jainam1234",
+    },
+    {
+      name: "Pranav Kapoor",
+      email: "kapoor.p@northeastern.edu",
+      password: "Pranav1234",
+    },
+    {
+      name: "Paurush Batish",
+      email: "batish.p@northeastern.edu",
+      password: "Paurush1234",
+    },
+    {
+      name: "Pranav Dhongade",
+      email: "dhongade.p@northeastern.edu",
+      password: "Pranav1234",
+    },
+  ];
+  
+  module.exports = {
+    userData,
+    carData,
+  };
+  

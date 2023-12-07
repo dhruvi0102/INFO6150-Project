7ddd28d7703c49c200c363e8446478ddf7d02bf8
dhruvi0102 <dhruvibavaria2@gmail.com>
2023-12-07 17:15:26 -0500

    Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-//import "./styles/App.css";
 
 import MainNav from "./shared/components/Navigation/MainNav";
-//import LandingPage from "./LandingPage/LandingPage";
+import LandingPage from "./LandingPage/LandingPage";
 import FleetPage from "./Fleet/pages/FleetPage";
 import OrdersPage from "./Orders/pages/OrdersPage";
 import FleetSearchPage from "./Fleet/pages/FleetSearchPage";
 import ThemeContextProvider from "./shared/contexts/ThemeContext";
 import AuthContextProvider from "./shared/contexts/AuthContext";
 import RentPage from "./Rent/pages/RentPage";
-//import ResetForm from "./User/components/ResetForm";
-//import ResetPage from "./User/pages/ResetPage";
 import PrivateRoute from "./shared/components/PrivateRoute/PrivateRoute";
-//import Auth from "./User/pages/Auth";
-//import About from "./About/a";
-//import Checkout from "./Checkout/Checkout";
+import About from "./About/a";
+import Checkout from "./Checkout/Checkout";
 
 function App() {
   return (
@@ -24,17 +20,14 @@ function App() {
         <Router>
           <MainNav />
           <Switch>
-            {/*
+            
             <Route exact path="/" component={LandingPage} />
-            <Route path="/auth/reset/:token" component={ResetForm} />
-            <Route path="/auth/reset" component={ResetPage} />
-  <Route path="/auth" component={Auth} />*/}
             <Route path="/fleet/sort/:name" component={FleetSearchPage} />
             <Route path="/fleet" component={FleetPage} />
-           {/* <Route path="/about" component={About} />*/}
+            <Route path="/about" component={About} />
             <PrivateRoute path="/my-orders/:id" component={OrdersPage} />
             <Route path="/rent/:id" component={RentPage} />
-            {/*<Route to="/checkout" component={Checkout} />*/}
+            <Route to="/checkout" component={Checkout} />
           </Switch>
         </Router>
       </ThemeContextProvider>
